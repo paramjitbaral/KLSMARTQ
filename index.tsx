6ec52a4da026489a3e0f9f9b5de2c0a46f762ffb
paramjitbaral <2300033156@kluniversity.in>
2026-06-02 11:40:31 +0530

    updated

--- index.tsx
+++ index.tsx
@@ -1,16 +1,19 @@
+/// <reference types="vite/client" />
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import { AppProvider } from './context/AppContext';
 
-// Disable all console outputs
-console.log = () => {};
-console.error = () => {};
-console.warn = () => {};
-console.info = () => {};
-console.debug = () => {};
-
+// Disable all console outputs only in production
+if (import.meta.env.PROD) {
+  console.log = () => {};
+  console.error = () => {};
+  console.warn = () => {};
+  console.info = () => {};
+  console.debug = () => {};
+}
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
